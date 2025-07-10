85c6f59419c4636c75138cff0abcec7271eda9fa
sm64isdabest <thiagopinheiro301007@gmail.com>
2025-07-10 10:46:32 -0300

    feat: User Model

--- Model/User.php
+++ Model/User.php
@@ -26,7 +26,7 @@ class User
     {
         try {
             // INSERÇÃO DE DADOS NA LINGUAGEM SQL
-            $sql = 'INSERT INTO user (user_fullname, email, password, created_at) VALUES (:user_fullname, :email, :password, NOW())';
+            $sql = 'INSERT INTO user .(user_fullname, email, password, created_at) VALUES (:user_fullname, :email, :password, NOW())';
 
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
@@ -44,6 +44,61 @@ class User
         } catch (PDOException $error) {
             // EXIBIR MENSAGEM DE ERRO COMPLETA E PARAR A EXECUÇÃO
             echo "Erro ao executar o comando" . $error->getMessage();
+            return false;
+        }
+    }
+
+    public function getUserByEmail($email)
+    {
+        try {
+            $sql = "SELECT * FROM user WHERE email = :email LIMIT 1";
+
+            $conn = $this->db->prepare($sql);
+
+            $conn->bindParam(":email", $email, PDO::PARAM_STR);
+
+            $conn->execute();
+
+            return $conn->fetch(PDO::FETCH_ASSOC);
+        } catch (PDOException $error) {
+            echo "Erro ao buscar informações: " . $error->getMessage();
+            return false;
+        }
+    }
+
+    // OBTER INFORMAÇÕES DO USUÁRIO
+
+    public function getUserInfo($user_fullname, $email)
+    {
+        try {
+            $sql = "SELECT user_fullname, email FROM user WHERE id = :id AND user_fullname = :user_fullname AND email = :email";
+
+            $conn = $this->db->prepare($sql);
+
+            $conn->bindParam(":id", $id, PDO::PARAM_INT);
+            $conn->bindParam(":user_fullname", $user_fullname, PDO::PARAM_INT);
+            $conn->bindParam(":email", $email, PDO::PARAM_INT);
+
+            $conn->execute();
+
+            /**
+             * fetch = querySelector();
+             * fetchAll = querySelectorAll();
+             * 
+             * FETCH_ASSOC:
+             * $user[
+             *  "user_fullname" => "teste",
+             *  "email" => "teste@example.com"
+             * ]
+             * 
+             * COMO OBTER INFORMAÇÕES:
+             * $user["user_fullname"];
+             */
+
+            return $conn->fetch(PDO::FETCH_ASSOC);
+
+        } catch (PDOException $error) {
+            echo "Erro ao buscar informações: " . $error->getMessage();
             return false;
         }
     }
