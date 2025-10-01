071a5c08b0f78ec3ca78be07c088947b680c5997
sm64isdabest <thiagopinheiro301007@gmail.com>
2025-10-01 12:21:59 -0300

    feat: UserTest.php + ImcTest.php

--- tests/ImcTest.php
+++ tests/ImcTest.php
@@ -4,6 +4,8 @@ use Controller\ImcController;
 
 use PHPUnit\Framework\TestCase;
 
+use Model\Imcs;
+
 class ImcTest extends TestCase
 {
 
@@ -12,9 +14,17 @@ class ImcTest extends TestCase
     // E A LÓGICA DA APLICAÇÃO
     private $imcController;
 
+    // ATRIBUTO PARA O FAKE DO BANCO DE DADOS
+    private $mockImcModel;
+
     protected function setUp(): void
     {
-        $this->imcController = new ImcController();
+        // CRIO O FAKE DO BANCO DE DADOS
+        $this->mockImcModel = $this->createMock(Imcs::class);
+
+        // PASSO ESSE FAKE PARA O CONTROLLER, ASSIM ME PERMITE UTILIZAR
+        // AS MESMAS FUNCIONALIDADES, SÓ QUE SEM MODIFICAR O BANCO DE DADOS REAL
+        $this->imcController = new ImcController($this->mockImcModel);
     }
 
     // Verificar o calculo do IMC
@@ -74,10 +84,89 @@ class ImcTest extends TestCase
         $this->assertEquals('Peso normal', $imcResult['BMIrange']);
     }
 
+    // OBTER O IMC E CLASSIFICAR COMO "Baixo peso"
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_returns_underweight_for_bmi()
+    {
+        $weight = 50;
+        $height = 1.75;
+
+        $imcResult = $this->imcController->calculateImc($weight, $height);
+
+        $this->assertStringNotContainsString('O peso e a altura devem conter valores positivos', $imcResult['BMIrange']);
+        $this->assertStringNotContainsString('Por favor, informe peso e altura para obter o seu IMC.', $imcResult['BMIrange']);
+        $this->assertEquals('Baixo peso', $imcResult['BMIrange']);
+    }
+
+    // OBTER O IMC E CLASSIFICAR COMO "Sobrepeso"
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_returns_overweight_for_bmi()
+    {
+        $weight = 85;
+        $height = 1.70;
+
+        $imcResult = $this->imcController->calculateImc($weight, $height);
+
+        $this->assertStringNotContainsString('O peso e a altura devem conter valores positivos', $imcResult['BMIrange']);
+        $this->assertStringNotContainsString('Por favor, informe peso e altura para obter o seu IMC.', $imcResult['BMIrange']);
+        $this->assertEquals('Sobrepeso', $imcResult['BMIrange']);
+    }
+
+    // OBTER O IMC E CLASSIFICAR COMO "Obesidade grau I"
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_returns_obesity_grade_one_for_bmi()
+    {
+        $weight = 95;
+        $height = 1.70;
+
+        $imcResult = $this->imcController->calculateImc($weight, $height);
+
+        $this->assertStringNotContainsString('O peso e a altura devem conter valores positivos', $imcResult['BMIrange']);
+        $this->assertStringNotContainsString('Por favor, informe peso e altura para obter o seu IMC.', $imcResult['BMIrange']);
+        $this->assertEquals('Obesidade grau I', $imcResult['BMIrange']);
+    }
+
+    // OBTER O IMC E CLASSIFICAR COMO "Obesidade grau II"
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_returns_obesity_grade_two_for_bmi()
+    {
+        $weight = 110;
+        $height = 1.75;
+
+        $imcResult = $this->imcController->calculateImc($weight, $height);
+
+        $this->assertStringNotContainsString('O peso e a altura devem conter valores positivos', $imcResult['BMIrange']);
+        $this->assertStringNotContainsString('Por favor, informe peso e altura para obter o seu IMC.', $imcResult['BMIrange']);
+        $this->assertEquals('Obesidade grau II', $imcResult['BMIrange']);
+    }
+
+    // OBTER O IMC E CLASSIFICAR COMO "Obesidade grau III"
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_returns_obesity_grade_three_for_bmi()
+    {
+        $weight = 130;
+        $height = 1.70;
+
+        $imcResult = $this->imcController->calculateImc($weight, $height);
+
+        $this->assertStringNotContainsString('O peso e a altura devem conter valores positivos', $imcResult['BMIrange']);
+        $this->assertStringNotContainsString('Por favor, informe peso e altura para obter o seu IMC.', $imcResult['BMIrange']);
+        $this->assertEquals('Obesidade grau III', $imcResult['BMIrange']);
+    }
+
     // Salvar o IMC
     #[PHPUnit\Framework\Attributes\Test]
     public function it_should_be_able_to_save_bmi()
     {
+        $imcResult = $this->imcController->calculateImc(68, 1.68);
+
+        $this->assertStringNotContainsString("Por favor, informe peso e altura para obter o seu IMC.", $imcResult['BMIrange']);
+
+        $this->mockImcModel->expects($this->once())->method('createImc')->with($this->equalTo(68), $this->equalTo(1.68))->willReturn(true);
+
+        $result = $this->imcController->saveIMC(68, 1.68, $imcResult['imc']);
+
+        $this->assertTrue($result);
     }
 }
 
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,8 +2,92 @@
 
 use PHPUnit\Framework\TestCase;
 
+use Controller\UserController;
+
+use Model\User;
+
 class UserTest extends TestCase
 {
-}
+    private $userController;
+    private $mockUserModel;
 
-?>
+    public function setUp(): void
+    {
+        $this->mockUserModel = $this->createMock(User::class);
+
+        $this->userController = new UserController($this->mockUserModel);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_be_able_to_create_user()
+    {
+        $this->mockUserModel->method('registerUser')->willReturn(true);
+
+        $userResult = $this->userController->createUser('Ana Luisa Santos', 'ana@example.com', '123456');
+
+        $this->assertTrue($userResult);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_be_able_to_sign_in()
+    {
+        $this->mockUserModel->method('getUserByEmail')->willReturn([
+            "id" => 1,
+            "user_fullname" => "Ana Luisa Santos",
+            "email" => "ana@example.com",
+            "password" => password_hash("123456", PASSWORD_DEFAULT)
+        ]);
+
+        $userResult = $this->userController->login("ana@example.com", "123456");
+
+        $this->assertTrue($userResult);
+
+        $this->assertEquals(1, $_SESSION["id"]);
+        $this->assertEquals("Ana Luisa Santos", $_SESSION["user_fullname"]);
+        $this->assertEquals("ana@example.com", $_SESSION['email']);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_shouldnt_login_with_invalid_credentials()
+    {
+        $this->mockUserModel->method('getUserByEmail')->willReturn([
+            "id" => 1,
+            "user_fullname" => "Ana Luisa Santos",
+            "email" => "ana@example.com",
+            "password" => password_hash("123456", PASSWORD_DEFAULT)
+        ]);
+
+        $userResult = $this->userController->login('ana@example.com', '12345');
+
+        $this->assertFalse($userResult);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+
+    public function it_should_be_able_to_check_user_by_email()
+    {
+        $this->mockUserModel->method('getUserByEmail')->willReturn([
+            "id" => 1,
+            "user_fullname" => "Ana Luisa Santos",
+            "email" => "ana@example.com",
+            "password" => '$2y$12$JFsscNxgtdEMaOBF6gILYuaIqSgn.5BaDL7cJMs4PCL1zyUnF5lnm'
+        ]);
+
+        $userResult = $this->userController->checkUserByEmail('ana@example.com');
+
+        $this->assertNotNull($userResult);
+
+        $this->assertEquals('ana@example.com', $userResult['email']);
+    }
+
+    #[\PHPUnit\Framework\Attributes\Test]
+    public function it_should_verify_if_is_logged_in()
+    {
+        $_SESSION['id'] = 1;
+
+        $userResult = $this->userController->isLoggedIn();
+
+        $this->assertTrue($userResult);
+    }
+
+}
